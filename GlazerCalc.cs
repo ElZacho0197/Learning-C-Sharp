2e1850af4774d6b5f1dd3f6e185d808bdfed223c
ElZacho0197 <ElZacho0197@users.noreply.github.com>
2020-06-21 19:58:19 -0400

    6/21

---
+++ GlazerCalc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class GlazerCalc
+    {
+        static public void glazerCalc()
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            Console.WriteLine("Enter the width of the window in feet.");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.WriteLine("Enter the height of the window in feet.");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = (2 * width) + (2 * height);
+            glassArea = (width * height) * 2;
+
+            Console.WriteLine("The length of wood is " + woodLength + " feet.");
+            Console.WriteLine("The area of the glass is " + glassArea + " square feet.");
+            Console.WriteLine("Press Enter to quit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Launcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Launcher
+    {
+        static void Main()
+        {
+            GlazerCalc.glazerCalc();
+        }
+    }
+}
